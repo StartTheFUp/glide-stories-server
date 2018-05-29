02787908f18939a7a1d5d91d7d2d3e9d77c539be
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-05-29 15:07:17 +0200

    create server, routes and mock

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const sip = require('./database/mock.json')
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.json('Hello World')
+})
+
+app.get('/mock', (req, res) => {
+  res.json(JSON.stringify(sip))
+})
+
+app.listen(5000, () => console.log('Port 5000'))
+
