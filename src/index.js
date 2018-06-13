b0b87b0dc5d6d77b4de893fec0b87477d5ef5fdd
Capucine Leclerc <capucine_49@hotmail.com>
2018-06-13 14:55:26 +0200

    correction bugs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const sip = require('./database/mock.json')
 const knex = require('./database/knex.js')
 
 app.use((req, res, next) => {
@@ -13,26 +12,11 @@ app.get('/', (req, res) => {
   res.json('Hello World')
 })
 
-// app.get('/mock', (req, res) => {
-//   res.json(sip)
-// })
-
 app.get('/sips', (req, res) => {
-  // knex.raw('select * from sips')
-  // .then((sips) => {
-  //   res.send(sips.rows)
-  // })
-
   knex.select().from('sips')
   .then((sips) => {
     res.send(sips)
   })
-
-  //test de select * from sips where id = 1 :
-  // knex.select().from('sips').where('id', 1)
-  // .then((sips) => {
-  //   res.send(sips)
-  // })
 })
 
 app.listen(5000, () => console.log('Port 5000'))
---
+++ src/migrations/20180611160349_sips-table.js
@@ -0,0 +1,18 @@
+
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('sips', (table) => {
+    table.increments()
+    table.string('type').notNullable()
+    table.string('title')
+    table.string('subtitle')
+    table.string('image', 500)
+    table.string('text', 300)
+
+  })
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('sips')
+
+
+};
