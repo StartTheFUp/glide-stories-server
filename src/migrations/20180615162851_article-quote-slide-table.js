64b96cd14b983b7f72324d67a95fdc1c932bd31b
Louise Foussat <louisefoussat@gmail.com>
2018-06-15 17:01:25 +0200

    create article quote table

---
+++ src/migrations/20180615162851_article-quote-slide-table.js
@@ -0,0 +1,18 @@
+
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('slides_article_quote', table => {
+    table.increments()
+    table.timestamp('created_at').defaultTo(knex.fn.now()) /* ask clement for the default... utility */
+    table.string('article_url', 500)
+    table.string('source_image', 500)
+    table.string('source_name', 500)
+    table.string('author_name', 500)
+    table.date('publication_date', 500)
+    table.integer('sip_id').unsigned()
+    table.foreign('sip_id').references('id').inTable('sips')
+  })
+}
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('slides_article_quote')
+}
