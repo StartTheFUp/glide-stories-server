cc3636038c819d626574befec329b5e4659709e1
Louise Foussat <louisefoussat@gmail.com>
2018-06-18 10:53:54 +0200

    add intro seed

---
+++ src/seeds/intro.js
@@ -0,0 +1,34 @@
+
+exports.seed = (knex, Promise) => {
+  // Deletes ALL existing entries
+  return knex('slides_intro').del()
+    .then(() => {
+      // Inserts seed entries
+      return knex('slides_intro').insert([
+        {
+          id: 1,
+          created_at: new Date(),
+          title: 'Test titre',
+          image_url: 'https://pbs.twimg.com/profile_images/685018005990993920/bQzEIcoY_normal.jpg',
+          subtitle: 'test sous titre',
+          sip_id: 1
+        },
+        {
+          id: 2,
+          created_at: new Date(),
+          title: 'Test titre 2',
+          image_url: 'https://pbs.twimg.com/profile_images/685018005990993920/bQzEIcoY_normal.jpg',
+          subtitle: 'test sous titre 2',
+          sip_id: 2
+        },
+        {
+          id: 3,
+          created_at: new Date(),
+          title: 'Test titre 3',
+          image_url: 'https://pbs.twimg.com/profile_images/685018005990993920/bQzEIcoY_normal.jpg',
+          subtitle: 'test sous titre 3',
+          sip_id: 1
+        }
+      ])
+    })
+}
