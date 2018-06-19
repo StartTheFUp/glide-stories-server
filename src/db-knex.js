cf977733e91facdb916fd27356390e0f53b9e8bf
Louise Foussat <louisefoussat@gmail.com>
2018-06-19 13:02:40 +0200

    update getSip() and getSlides() function => get all slides refered to a sip according to its id, get a slide in its table according to its id

--- src/db-knex.js
+++ src/db-knex.js
@@ -1,29 +1,37 @@
 const knex = require('./database/knex.js')
 
-const getSlides = async slideType => {
-  const slidesText = await knex
-    .select().table(`${slideType}`)
-    // .then(slides => console.log('slides :', slides))
-    .catch(err => console.log(err))
 
-  return slidesText
+const slideTypes = {
+  text: 'slides_text',
+  intro: 'slides_intro',
+  image: 'slides_image',
+  tweet: 'slides_tweet_quote',
+  article: 'slides_article_quote',
+  callToAction: 'slides_call_to_action',
 }
+const slideTypesEntries = Object.entries(slideTypes)
+/*
 
-const getSip = async () => {
-  const slidesText = await getSlides('slides_text')
-  const slidesImage = await getSlides('slides_image')
-  const slidesIntro = await getSlides('slides_intro')
-  const slidesArticleQuote = await getSlides('slides_article_quote')
-  const slidesTweetQuote = await getSlides('slides_tweet_quote')
-  const slidesCallToAction = await getSlides('slides_call_to_action')
+const dbTypesToJS = jsTypesToDB)
+  .reduce((acc, [value, key]) => {
+    acc[key] = value
+    return acc
+  }, {})
 
-  const sip = {text: slidesText, image: slidesImage}
+const slideTypeValues = Object.values(jsTypesToDB)
+*/
+
+const getSlidesBySipId = (type, id) => knex
+  .select()
+  .table(type)
+  .where('sip_id', id)
+
+const getSlideById = (slideType, slideId) => knex
+    .select()
+    .table(slideType)
+    .where('id', slideId)
 
   return sip
 }
 
-getSip().then(sip => console.log('sip :', sip))
 
-//
-// let slidesText = []
-// getSlidesText().then(slides => slidesText = slides))
