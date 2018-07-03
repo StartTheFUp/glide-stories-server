2c09f91da883c7f39f130fcc3bab3e66474ad86d
Louise Foussat <louisefoussat@gmail.com>
2018-07-03 17:08:09 +0200

    new migration -> change publication_date data-type for slides tweet -> timestamp

---
+++ src/migrations/20180703003825_changeTweetDate.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.alterTable('slides_tweet_quote', table => {
+    table.timestamp('publication_date').alter()
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.alterTable('slides_tweet_quote', table => {
+    table.date('publication_date').alter()
+  })
+}
